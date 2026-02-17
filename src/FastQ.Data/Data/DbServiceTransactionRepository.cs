2c349157cb240b695945aed4a5833bd0f7c644ef
softauratech-art <softauratech@gmail.com>
2026-02-17 19:15:48 +0600

    save service info fix

--- src/FastQ.Data/Data/DbServiceTransactionRepository.cs
+++ src/FastQ.Data/Data/DbServiceTransactionRepository.cs
@@ -39,26 +39,14 @@ namespace FastQ.Data.Db
         public void SaveServiceInfo(char srcType, long srcId, string webexUrl, string notes, string stampUser)
         {
             using (var conn = DataAccess.Open())
-            using (var cmd = DataAccess.CreateStoredProc(conn, "FT_PROCS_IDU.SAVE_SERVICE_INFO"))
+            using (var cmd = DataAccess.CreateStoredProc(conn, "FQ_PROCS.SAVE_SERVICE_INFO"))
             {
                 DataAccess.AddParam(cmd, "p_src_type", srcType.ToString(), DbType.String);
                 DataAccess.AddParam(cmd, "p_src_id", srcId, DbType.Int64);
                 DataAccess.AddParam(cmd, "p_webex_url", webexUrl, DbType.String);
                 DataAccess.AddParam(cmd, "p_notes", notes, DbType.String);
                 DataAccess.AddParam(cmd, "p_stampuser", stampUser, DbType.String);
-
-                var outMsg = cmd.CreateParameter();
-                outMsg.ParameterName = "p_outmsg";
-                outMsg.Direction = ParameterDirection.Output;
-                outMsg.DbType = DbType.String;
-                outMsg.Size = 4000;
-                cmd.Parameters.Add(outMsg);
-
                 cmd.ExecuteNonQuery();
-
-                var message = outMsg.Value == DBNull.Value ? string.Empty : outMsg.Value?.ToString();
-                if (!string.IsNullOrWhiteSpace(message))
-                    throw new InvalidOperationException(message);
             }
         }
 
